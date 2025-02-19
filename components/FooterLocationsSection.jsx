0da4ee42decfae047172ba8fd163a6a9040ae8dd
Gitit2meBaby <thisworldofdans@gmail.com>
2025-02-19 13:43:40 +0700

    changed to nofollow links

--- components/FooterLocationsSection.jsx
+++ components/FooterLocationsSection.jsx
@@ -103,7 +103,11 @@ export default function FooterLocationsSection() {
                 <div className={styles.dropdownContent}>
                   {Object.entries(locations).map(([city, url]) => (
                     <div key={city} className={styles.cityItem}>
-                      <Link href={url} className={styles.cityLink}>
+                      <Link
+                        rel="nofollow noopener noreferrer"
+                        href={url}
+                        className={styles.cityLink}
+                      >
                         {city}
                       </Link>
                     </div>
