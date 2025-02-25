59fed09eaae973115f296b61c9c002364fd5654c
Gitit2meBaby <thisworldofdans@gmail.com>
2025-02-25 13:37:01 +0700

    added ahrefs

--- app/services/microsoft-power-platform/microsoft-power-apps/layout.js
+++ app/services/microsoft-power-platform/microsoft-power-apps/layout.js
@@ -3,14 +3,14 @@ import React from "react";
 export const metadata = {
   // Basic metadata
   title:
-    "Power Apps Development & Consulting Services | Power Platform Experts",
+    "Power Apps Consulting & Development Services | Power Platform Experts",
   description:
     "Custom Power Apps solutions for streamlined operations, automated workflows, and enhanced business efficiency.",
 
   // OpenGraph
   openGraph: {
     title:
-      "Power Apps Development & Consulting Services | Power Platform Experts",
+      "Power Apps Consulting & Development Service | Power Platform Experts",
     description:
       "Custom Power Apps solutions for streamlined operations, automated workflows, and enhanced business efficiency.",
     url: "https://www.powerplatformexperts.com.au/services/microsoft-power-platform/microsoft-power-apps",
@@ -18,7 +18,7 @@ export const metadata = {
   },
 
   // Additional metadata
-  keywords: ["custom Power Apps workflow automation"],
+  keywords: ["Power Apps consulting Australia"],
   alternates: {
     canonical: "/services/microsoft-power-platform/microsoft-power-apps",
     alternate: [
