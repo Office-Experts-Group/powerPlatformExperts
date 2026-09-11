15212be9e2d8e8f2cfd1ca61aa95016a00670398
Gitit2meBaby <thisworldofdans@gmail.com>
2026-09-11 14:47:57 +0700

    typo2

--- app/(components)/Testimonials.jsx
+++ app/(components)/Testimonials.jsx
@@ -10,7 +10,10 @@ import { filterAndSortTestimonials } from "../../utils/filterTestimonials";
 
 const Testimonials = ({ testimonials }) => {
   const getTestimonials = (count = 10) => {
-    const sortedTestimonials = filterAndSortTestimonials(testimonials, "word");
+    const sortedTestimonials = filterAndSortTestimonials(
+      testimonials,
+      "powerplatform",
+    );
 
     // Filter out testimonials without images or content
     const validTestimonials = sortedTestimonials.filter(
